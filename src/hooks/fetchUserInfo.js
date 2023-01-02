96d4298672400f6b6d96b2dd15dd8d6c869e0940
ZiuChen <457353192@qq.com>
2023-01-02 18:45:23 +0800

    fix: 修正代理服务器配置问题

--- src/hooks/fetchUserInfo.js
+++ src/hooks/fetchUserInfo.js
@@ -1,6 +1,5 @@
 export default async function fetchUserInfo(uid) {
-  console.log(window.location.origin)
-  const url = `${window.location.origin}/api/v1/user/get?spider=0&user_id=${uid}&not_self=1&need_badge=1`
+  const url = `/api/user_api/v1/user/get?spider=0&user_id=${uid}&not_self=1&need_badge=1`
   return new Promise((resolve, reject) => {
     fetch(url)
       .then((res) => res.json())
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,14 @@ import { createStyleImportPlugin } from 'vite-plugin-style-import'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3030
+    port: 3030,
+    proxy: {
+      '/api': {
+        target: 'https://api.juejin.cn',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   },
   css: {
     preprocessorOptions: {
